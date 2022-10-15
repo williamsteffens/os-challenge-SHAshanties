1393679c47daac66efe9a562cd4faa59954129c0
williamsteffens <william.f.f.steffens@gmail.com>
2022-10-15 10:21:57 +0200

    ADD/EDIT: Changed src strucutre

--- src/main.c
+++ src/main.c
@@ -1,23 +1,20 @@
-#define DEBUG 0
+#define DEBUG 1
 
 #include <stdio.h>
 #include <stdlib.h>
-// TODO: check for the need of the includes
+#include <string.h>
 #include <errno.h>
-
 #include <unistd.h>
-
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/select.h>
 #include <netdb.h>
 #include <netinet/in.h>
 
-// is included in messages.h
-// #include <openssl/sha.h>
-
-#include <string.h>
+#include <inttypes.h>
 
 #include "messages.h"
+#include "hashTable.h"
 
 
 
@@ -33,57 +30,100 @@ typedef union _response {
    uint8_t bytes[8];
 } response; 
 
-
-unsigned short port; 
-int listen_sd; 
-int on = 1;
-int conn_sd, clilen;
 uint8_t buffer[PACKET_REQUEST_SIZE];
-struct pollfd fds[200];
-struct sockaddr_in serv_addr, cli_addr;
-// test this?
-struct addrinfo; 
 pid_t pid;
-int timeout;
-int poll_status; 
 
-uint64_t answer;
+hashTable_t* hashtable;
 
 
 
-int bruteForceHash(uint8_t hash[SHA256_DIGEST_LENGTH], uint8_t guessHash[SHA256_DIGEST_LENGTH]);
-void SHASolver(int conn_sd);
+int setupServer(short port, int backlog);
+int acceptNewConnection(int servSocket);
+int hashMatches(uint8_t hash[SHA256_DIGEST_LENGTH], uint8_t guessHash[SHA256_DIGEST_LENGTH]);
+void SHASolver(int cliSocket);
 
 
 
 int main(int argc, char *argv[]) {
    if (argc != 2) {
-      fprintf(stderr, "Incorrect usage. Usage: %listen_sd port\n", argv[0]);
-      exit(-1);
+      fprintf(stderr, "Incorrect usage. Usage: %s port\n", argv[0]);
+      exit(EXIT_FAILURE);
    }   
 
    // First Argument should be the socket
-   port = (unsigned short) atoi(argv[1]);
+   short port = (short) atoi(argv[1]);
    
+   // TODO: Look into the backlog stuff
+   int servSocket = setupServer(port, 100);
+
+   fd_set currentSockets, readySockets;
+   FD_ZERO(&currentSockets);
+   FD_SET(servSocket, &currentSockets);
+   int maxSocketSoFar = servSocket; 
+
+   hashtable = htInit();
+
+   while (1) {
+      readySockets = currentSockets; 
+      
+      // TODO: max + 1 ??? 
+      if (select(maxSocketSoFar + 1, &readySockets, NULL, NULL, NULL) < 0) {
+         perror("[server] error select()");
+         exit(EXIT_FAILURE);
+      }
+
+      for (int i = 0; i <= maxSocketSoFar + 1; ++i)
+         if (FD_ISSET(i, &readySockets))
+            if (i == servSocket) {
+               // There's data to read
+               int cliSocket = acceptNewConnection(servSocket);
+               FD_SET(cliSocket, &currentSockets);
+               if (cliSocket > maxSocketSoFar)
+                  maxSocketSoFar = cliSocket;
+            } 
+            else {
+               SHASolver(i);
+               FD_CLR(i, &currentSockets);
+            }
+
+      // Accept connection from client
+
+      // if ((pid = fork()) < 0)  {
+      //    perror("[server] error fork()");
+      //    exit(EXIT_FAILURE);
+      // } 
+
+      // if (pid == 0) {
+      //    close(servSocket);
+      //    SHASolver(cliSocket);
+      //    exit(0);
+      // } else {
+      //    close(cliSocket);
+      // } 
+   }
+	
+   printf("[server] Server ended successfully\n");
+      
+   return EXIT_SUCCESS;
+}
+
+
+
+int setupServer(short port, int backlog) {
+   int servSocket;
+   struct sockaddr_in serv_addr;
+
    // Get socket for accepting connections
-   if ((listen_sd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-      perror("[server] socket() failed");
-      close(listen_sd);
-      exit(-1);
+   if ((servSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+      perror("[server] error socket()");
+      exit(EXIT_FAILURE);
    }
 
    // Set socket option to reuse the addr/port without waiting for it to be release 
-   if ((setsockopt(listen_sd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on))) < 0) {
-      perror("[server] setsockopt() failed");
-      close(listen_sd);
-      exit(-1);
-   }
-
-   // Set socket to be non-blocking. All clientsockets will inherit this from the listening socket.
-   if ((ioctl(listen_sd, FIONBIO, &on)) < 0) {
-      perror("[server] ioctl() failed");
-      close(listen_sd);
-      exit(-1);
+   int optVal = 1;
+   if ((setsockopt(servSocket, SOL_SOCKET, SO_REUSEADDR, &optVal, sizeof(optVal))) < 0) {
+      perror("[server] error setsockopt()");
+      exit(EXIT_FAILURE);
    }
 
    // Bind socket to server addr
@@ -93,97 +133,47 @@ int main(int argc, char *argv[]) {
    serv_addr.sin_addr.s_addr = INADDR_ANY;
    serv_addr.sin_port = htons(port);
    
-   if ((bind(listen_sd, (struct sockaddr *) &serv_addr, sizeof(serv_addr))) < 0) {
-      perror("[server] bind() failed");
-      close(listen_sd);
-      exit(-1);
+   if ((bind(servSocket, (struct sockaddr *) &serv_addr, sizeof(serv_addr))) < 0) {
+      perror("[server] error bind()");
+      exit(EXIT_FAILURE);
    }
    
-   // Listen for connection, backlog = 100
-   if ((listen(listen_sd,100)) != 0) {
-      perror("[server] listen() failed");
-      close(listen_sd);
-      exit(-1);
+   // Listen for connection, backlog = 5
+   if ((listen(servSocket, backlog)) != 0) {
+      perror("[server] error listen()");
+      exit(EXIT_FAILURE);
    }
    printf("[server] Listening on port %d...\n", port);
-   
-   // Initialize the pollfd struct 
-   memset(fds, 0 , sizeof(fds));
 
-   // Set up the initial listening socket
-   fds[0].fd = listen_sd;
-   fds[0].events = POLLIN;
+   return servSocket;
+}
 
-   // Set timeout to 3 minutes; if no activity for 3 min, the program will terminate
-   timeout = (3 * 60 * 1000);
+int acceptNewConnection(int servSocket) {
+   int cliSocket;
+   struct sockaddr_in cli_addr;
+   int clilen = sizeof(cli_addr);
 
-   // Accept connection from client
-   // clilen = sizeof(cli_addr);
-
-   while (1) {
-      // Wait for poll and wait 3 minuntes for it to complete
-      printf("Waiting on poll()...\n");
-      poll_status = poll(fds, nfds, timeout)
-      if (poll_status < 0) {
-         perror("[server] poll() failed");
-         close(listen_sd);
-         exit(-1);
-      }
-      else if (poll_status == 0) {
-         printf("[server] poll() timeout. Terminating server... \n");
-         close(listen_sd);
-         break; 
-      }
-
-      // One or more descriptors are readable. Iterate and find out which ones
-      
-
-
-
-
-
-
-
-      if ((conn_sd = accept(listen_sd, (struct sockaddr *)&cli_addr, &clilen)) == -1) {
-         perror("[server] accept() failed");
-         close(listen_sd);
-         exit(-1);
-      }
-
-      if ((pid = fork()) < 0)  {
-         perror("[server] fork() failed");
-         close(listen_sd);
-         exit(-1);
-      } 
-
-      if (pid == 0) {
-         close(listen_sd);
-         SHASolver(conn_sd);
-         exit(-1);
-      } else {
-         close(conn_sd);
-      } 
+   if ((cliSocket = accept(servSocket, (struct sockaddr *)&cli_addr, &clilen)) == -1) {
+      perror("[server] error accept()");
+      exit(EXIT_FAILURE);
    }
-	
-   printf("[server] Server ended successfully\n");
-      
-   return 0;
+
+   return cliSocket;
 }
 
-int bruteForceHash(uint8_t hash[SHA256_DIGEST_LENGTH], uint8_t guessHash[SHA256_DIGEST_LENGTH]) {
-   for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i)
-      if (hash[i] != guessHash[i])
-         return 0;
-   
-   return 1;
+int hashMatches(uint8_t hash[SHA256_DIGEST_LENGTH], uint8_t guessHash[SHA256_DIGEST_LENGTH]) {
+   if (memcmp(hash, guessHash, SHA256_DIGEST_LENGTH) == 0)
+      return 1;
+   else 
+      return 0;
 }
 
-void SHASolver(int conn_sd) {
+void SHASolver(int cliSocket) {
    // Read request from client
    bzero(buffer, PACKET_REQUEST_SIZE);
-   if ((read(conn_sd, buffer, PACKET_REQUEST_SIZE)) == -1) {
-      perror("[server] read() failed");
-   exit(-1);
+   if ((read(cliSocket, buffer, PACKET_REQUEST_SIZE)) == -1) {
+      perror("[server] error read()");
+      exit(EXIT_FAILURE);
    }
 
    // Zerolize vars
@@ -191,6 +181,18 @@ void SHASolver(int conn_sd) {
 
    // Extrat data from request/msg
    memcpy(&req.hash, buffer + PACKET_REQUEST_HASH_OFFSET, SHA256_DIGEST_LENGTH);
+
+   // respond before anything else if we have the hash in the cache
+   if (htContainsKey(hashtable, req.hash)) {
+      uint64_t ans = htGet(hashtable, req.hash);
+      if ((write(cliSocket, &ans, sizeof(uint64_t))) == -1) {
+         perror("[server] error write()");
+         exit(EXIT_FAILURE);
+      }
+      printf("\n\thashtabled, biatch\n");
+      return;
+   }
+
    memcpy(&req.start, buffer + PACKET_REQUEST_START_OFFSET, 8);
    memcpy(&req.end, buffer + PACKET_REQUEST_END_OFFSET, 8);
    req.prio = buffer[PACKET_REQUEST_PRIO_OFFSET];
@@ -215,15 +217,20 @@ void SHASolver(int conn_sd) {
    response res = {0};
    for (res.num = req.start; res.num < req.end; ++res.num) {
       SHA256(res.bytes, sizeof(uint64_t), guessHash);
-      if (bruteForceHash(req.hash, guessHash)) {
+      if (hashMatches(req.hash, guessHash)) {
+         #if DEBUG
+            printf("ans: %" PRIu64 "\n\n", res.num);
+         #endif
          res.num = htobe64(res.num);
+         htSet(hashtable, req.hash, res.num);
          break;
       }
    }
 
    // Write response to client
-   if ((write(conn_sd, res.bytes, sizeof(uint64_t))) == -1) {
-      perror("[server] write() failed");
-      exit(-1);
+   if ((write(cliSocket, res.bytes, sizeof(uint64_t))) == -1) {
+      perror("[server] error write()");
+      exit(EXIT_FAILURE);
    }
+
 }
